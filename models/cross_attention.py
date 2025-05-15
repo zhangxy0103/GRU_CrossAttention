c06647224aff04fa03a787ffed5904a78a2a9f01
zhangxy <127104692+zhangxy0103@users.noreply.github.com>
2025-05-15 11:32:16 +0800

    cross_attention.py

--- models/cross_attention.py
+++ models/cross_attention.py
@@ -1,103 +1,30 @@
-import torch
-from torch import nn
-from torch.nn import functional as F
-
-
-class ASPPConv(nn.Sequential):
-    def __init__(self, in_channels, out_channels, dilation):
-        modules = [
-            # 空洞卷积，通过调整dilation参数来捕获不同尺度的信息
-            nn.Conv1d(in_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False),
-            nn.BatchNorm1d(out_channels),  # 批量归一化
-            nn.ReLU()  # ReLU激活函数
-        ]
-        super(ASPPConv, self).__init__(*modules)
-
-
 class CrossAttention(nn.Module):
-
-    def __init__(
-            self,
-            dim,
-            num_heads=8,
-            qkv_bias=False,
-            qk_scale=None,
-            attn_drop=0.0,
-            proj_drop=0.0,
-    ):
-        super().__init__()
+    def __init__(self, in_dim1, in_dim2, k_dim, v_dim, num_heads):
+        super(CrossAttention, self).__init__()
         self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.k_dim = k_dim
+        self.v_dim = v_dim
 
-        self.q = nn.Linear(dim, dim, bias=qkv_bias)
-        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
+        self.proj_q1 = nn.Linear(in_dim1, k_dim * num_heads, bias=False)
+        self.proj_k2 = nn.Linear(in_dim2, k_dim * num_heads, bias=False)
+        self.proj_v2 = nn.Linear(in_dim2, v_dim * num_heads, bias=False)
+        self.proj_o = nn.Linear(v_dim * num_heads, in_dim1)
 
-    def forward(self, q, kv, key_mask=None):
-        N, C = kv.shape[1:]
-        QN = q.shape[1]
-        q = self.q(q).reshape([-1, QN, self.num_heads,
-                               C // self.num_heads]).transpose(1, 2)
-        q = q * self.scale
-        k, v = self.kv(kv).reshape(
-            [-1, N, 2, self.num_heads,
-             C // self.num_heads]).permute(2, 0, 3, 1, 4)
+    def forward(self, x1, x2, mask=None):
+        batch_size, seq_len1, in_dim1 = x1.size()
+        seq_len2 = x2.size(1)
 
-        attn = q.matmul(k.transpose(2, 3))
+        q1 = self.proj_q1(x1).view(batch_size, seq_len1, self.num_heads, self.k_dim).permute(0, 2, 1, 3)
+        k2 = self.proj_k2(x2).view(batch_size, seq_len2, self.num_heads, self.k_dim).permute(0, 2, 3, 1)
+        v2 = self.proj_v2(x2).view(batch_size, seq_len2, self.num_heads, self.v_dim).permute(0, 2, 1, 3)
 
-        if key_mask is not None:
-            attn = attn + key_mask.unsqueeze(1)
+        attn = torch.matmul(q1, k2) / self.k_dim ** 0.5
 
-        attn = F.softmax(attn, -1)
-        if not self.training:
-            self.attn_map = attn
-        attn = self.attn_drop(attn)
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
 
-        x = (attn.matmul(v)).transpose(1, 2).reshape((-1, QN, C))
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
+        attn = torch.softmax(attn, dim=-1)
+        output = torch.matmul(attn, v2).permute(0, 2, 1, 3).contiguous().view(batch_size, seq_len1, -1)
+        output = self.proj_o(output)
 
-
-class MultiScaleCrossAttention(nn.Module):
-    def __init__(self, in_channels, atrous_rates, out_channels, num_heads=8, qkv_bias=False, qk_scale=None,
-                 attn_drop=0.0, proj_drop=0.0):
-        super().__init__()
-        out_channels = out_channels  # 输出通道数
-        modules = []
-        self.aa = ASPPConv(in_channels, out_channels, 6)
-        # 根据不同的膨胀率添加空洞卷积模块
-        for rate in atrous_rates:
-            modules.append(ASPPConv(in_channels, out_channels, rate))
-
-        self.convs = nn.ModuleList(modules)
-
-        self.cr = CrossAttention(dim=out_channels,
-                                 num_heads=num_heads,
-                                 qkv_bias=qkv_bias,
-                                 qk_scale=qk_scale,
-                                 attn_drop=attn_drop,
-                                 proj_drop=proj_drop,
-                                 )
-
-    def forward(self, x):
-        for conv in self.convs:
-            x = self.cr(x, conv(x.permute(0, 2, 1)).permute(0, 2, 1))
-        return x
-
-#test
-# if __name__ == '__main__':
-#     model = MultiScaleCrossAttention(in_channels=512,
-#                                      out_channels=512,
-#                                      atrous_rates=[6, 12, 18],
-#                                      num_heads=8,
-#                                      qkv_bias=False,
-#                                      qk_scale=None,
-#                                      attn_drop=0.0,
-#                                      proj_drop=0.0, )
-#     src = torch.rand(10, 32, 512)
-#     out = model(src)
-#     print(out.shape)
+        return output
